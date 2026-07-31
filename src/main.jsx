3992a41d53772d7840ef2306d45ae986963c76f0
lgoenaga <lgoenaga@cesde.net>
2026-07-31 16:22:31 -0500

    Corregir ruta inicial GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-
 import App from "./App";
 import "./index.css";
+
+if (
+  window.location.pathname === "/project-sistema-gestion-cursos-supabase/"
+  && !window.location.hash
+) {
+  window.location.replace(
+    "/project-sistema-gestion-cursos-supabase/#/"
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
